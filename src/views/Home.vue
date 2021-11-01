b1361bf715a9e17a722f806453c406ae879db932
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-11-01 12:57:04 +0200

    Replaced with a new syntax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae fuga
+      aliquam dolores! Quam sequi id provident dicta, sunt laudantium, quaerat
+      ipsa aperiam, nesciunt atque facilis aliquid repellendus rem amet
+      consectetur?
+    </p>
   </div>
 </template>
 
